33693ee3c70677594b54ff8fd8f490050c5593f5
ravi.bidnoor <ravi.bidnoor@JKTDKLS008.JKTECH>
2017-08-29 15:28:44 +0530

    WriteData into Excelsheet

---
+++ src/datadriventesting/WrittingData.java
@@ -0,0 +1,44 @@
+package datadriventesting;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class WrittingData {
+	@Test
+	public void writting() throws IOException
+	{
+		System.out.println("Testing started.. for to open JKT map and taking SS");
+		System.setProperty("webdriver.geckodriver.driver", "‪D:\\Automation Projects\\geckodriver-v0.17.0-win64.zip");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		System.setProperty("webdriver.chrome.driver", "D:\\Automation Projects\\chromedriver.exe");
+		WebDriver wd=new ChromeDriver(options);
+		System.out.println("Test Starting");
+		wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		wd.manage().window().maximize(); 
+	    wd.get("http://www.goole.com");
+	    String s=wd.getPageSource();
+		File file=new File("D:\\Automation Projects\\TestData.xlsx");
+		FileInputStream fis=new FileInputStream(file);
+		XSSFWorkbook wb=new XSSFWorkbook(fis);
+		XSSFSheet sheet=wb.getSheetAt(0);	
+		  Cell searchText = sheet.getRow(0).getCell(0);
+		  searchText.setCellValue(s);
+		 FileOutputStream fos=new FileOutputStream("D:\\Automation Projects\\TestData.xlsx");
+		 wb.write(fos);
+		 wb.close();
+	}
+
+}
